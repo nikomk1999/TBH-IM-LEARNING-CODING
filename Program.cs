8afa18b20400cec1ccebfd75283c7d1f2d1aeeb1
nikomk1999 <39340362+nikomk1999@users.noreply.github.com>
2021-05-06 09:49:03 +0300

    this is now only for c#

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TBH_IM_LEARNING_CODING
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
