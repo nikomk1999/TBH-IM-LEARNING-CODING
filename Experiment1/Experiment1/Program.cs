4dd751f6aadc1748db346184b2cedb0e470ae721
nikomk1999 <39340362+nikomk1999@users.noreply.github.com>
2021-05-06 14:29:17 +0300

    a

--- Experiment1/Experiment1/Program.cs
+++ Experiment1/Experiment1/Program.cs
@@ -5,8 +5,23 @@ namespace Experiment1
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   
+            byte number = 1;
+            byte number2 = 128;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'B';
+            string FirstName = "Niko";
+            bool IsWorking = false;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(FirstName);
+            Console.WriteLine(IsWorking);
+            //number = number + number2;
+            Console.WriteLine(number + "yo cool");
         }
     }
 }
