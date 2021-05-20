6e369cdfeb7b734f3b04cd3c006321845948b05b
nikomk1999 <39340362+nikomk1999@users.noreply.github.com>
2021-05-20 16:32:30 +0300

    a

---
+++ cs/TimerManForm/TimerManForm/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace TimerManForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbTime = new System.Windows.Forms.Label();
+            this.Timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lbTime
+            // 
+            this.lbTime.AutoSize = true;
+            this.lbTime.Location = new System.Drawing.Point(12, 9);
+            this.lbTime.Name = "lbTime";
+            this.lbTime.Size = new System.Drawing.Size(30, 13);
+            this.lbTime.TabIndex = 0;
+            this.lbTime.Text = "Time";
+            this.lbTime.Visible = false;
+            this.lbTime.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Timer1
+            // 
+            this.Timer1.Interval = 1000;
+            this.Timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 150);
+            this.Controls.Add(this.lbTime);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTime;
+        private System.Windows.Forms.Timer Timer1;
+    }
+}
+
---
+++ cs/TimerManForm/TimerManForm/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace TimerManForm
+{
+    public partial class Form1 : Form
+    {
+        private bool RtoC1;
+        public Form1()
+        {
+            InitializeComponent();
+            Timer1.Start();
+            BOOT Form2 = new BOOT();
+            Form2.Show();
+            TimeVisible();
+        }
+
+        private async void TimeVisible()
+        {
+            RtoC1 = false;
+            await Task.Delay(2000);
+            lbTime.Visible = true;
+            RtoC1 = true;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            this.lbTime.Text = DateTime.Now.ToString();
+        }
+    }
+}
